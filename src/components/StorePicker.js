866b927c8d1bc980d14ab8433fa0697dbba3860d
ethankhoa <ethan.huynh@gmail.com>
2017-05-16 10:18:36 +1000

    updates modules

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    // comment here
+    return (
+      <form className="store-selector">
+        { /* Hello */ }
+        <h2>Please Enter A Store</h2>
+        <input type="text" require placeholder="Store Name" />
+        <button type="submit">Visit Store -></button>
+      </form>
+    )
   }
 }
 
